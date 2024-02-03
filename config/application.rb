d084818f9817bd0acc8c799c92df381747e91c7b
farshidmarefat <farshid.marefat@example.com>
2024-02-03 23:24:57 +0330

    initial commite, edit application.rb, database.yml

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module BookshopPortfolioRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.encoding = 'utf-8'
   end
 end

3f3f182d7cb44fefc7e9abd0eb838112a5f5e55e
farshidmarefat <farshid.marefat@example.com>
2024-03-10 00:48:45 +0330

    add audited gem and migration, fix app_meta controller structure

--- app/controllers/api/v1/app_meta_controller.rb
+++ app/controllers/api/v1/app_meta_controller.rb
@@ -1,7 +1,8 @@
 module Api
   module V1
     class AppMetaController < ApplicationController
-      before_action :set_app_meta, only: [:show, :edit, :update, :destroy]
+      skip_before_action :verify_authenticity_token
+      before_action :set_app_meta, only: [:show, :update, :destroy]
 
       def index
         @app_metum = AppMetum.all
@@ -15,38 +16,27 @@ module Api
       end
 
       def create
-        @app_metum = AppMetum.new(app_meta_params)
-        if @app_metum.save
-          redirect_to @app_metum, notice: 'AppMeta was successfully created.'
-        else
-          render :new
-        end
-      end
-
-      def edit
+        @app_metum = ::AppMetum.create(app_meta_params)
+        render :show
       end
 
       def update
-        if @app_metum.update(app_meta_params)
-          redirect_to @app_metum, notice: 'AppMeta was successfully updated.'
-        else
-          render :edit
-        end
+        @app_metum.update_attributes(app_meta_params)
+        render :show
       end
 
       def destroy
-        @app_metum.destroy
-        redirect_to app_meta_url, notice: 'AppMeta was successfully destroyed.'
+        @app_metum.destroy!
       end
 
       private
 
       def set_app_meta
-        @app_metum = AppMetum.find(params[:id])
+        @app_metum = ::AppMetum.find(params[:id])
       end
 
       def app_meta_params
-        params.require(:app_meta).permit(:shop_name, :first_phone, :second_phone, :instagram, :telegram, :whatsapp, :facebook, :linkedin, :twitter, :youtube, :aparat, :email, :address, :latitude, :longitude, :terms, :about_us, :meta, :description)
+        params.permit(:shop_name, :first_phone, :second_phone, :instagram, :telegram, :whatsapp, :facebook, :linkedin, :twitter, :youtube, :aparat, :email, :address, :latitude, :longitude, :terms, :about_us, :meta, :description)
       end
     end
   end
